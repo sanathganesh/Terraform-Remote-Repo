84714edac3fce96ea7458597bd6cfa331a2669f2
sanathganesh <140713601+sanathganesh@users.noreply.github.com>
2023-07-27 17:07:42 +0530

    Push to Remote Repo

---
+++ Terraform Scripts/terraform.tf
@@ -0,0 +1,39 @@
+terraform {
+
+  required_providers {
+
+    azurerm = {
+
+      version = "= 3.66.0"
+
+    }
+
+  }
+
+}
+provider "azurerm" {
+
+features {}
+
+}
+resource "azurerm_resource_group" "resourcegroup" {
+
+  name     = "CUR-TEST-SYSLOG-WE-RG-TEST"
+
+  location = "West Europe"
+
+}
+
+ 
+
+resource "azurerm_virtual_network" "Vnet" {
+
+  name                = "CUR-TEST-SYSLOG-WE-VNET-TEST"
+
+  address_space       = ["10.0.0.0/18"]
+
+  location            = azurerm_resource_group.resourcegroup.location
+
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+
+}
